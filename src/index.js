3d5981c40ed5834d55929f1d2cfa84fbc2b45a4c
AnnaOW <anna.wierzbicka1996@gmail.com>
2024-01-30 20:50:07 +0100

    Integrated SheCodes API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
+  console.log("Poem generated");
 
   new Typewriter("#polish-poem", {
-    strings: "Oboje są przekonani, że połączyło ich uczucie nagłe.",
+    strings: response.data.answer,
     autoStart: true,
     delay: 80,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "9a4cbff04f4e654ca4teaa03bc88aoaf";
+  let prompt = `User instructions: Generate a poem in Polish language about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic poem expert and love to write short poems. Your mission is to generatea 4 line poem in basic HTML. Always separate each line with <br />. Sign the poem as SheCodes AI inside the <strong> element at the end of the poem, and never at the beginning. Do not include any title to the poem. Make sure to follow the user instructions.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem, please wait...");
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
