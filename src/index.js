1f3a62baeafdb283f54a81315d0a042facccb4af
AnnaOW <anna.wierzbicka1996@gmail.com>
2024-01-31 20:17:25 +0100

    Improved UX

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 function displayPoem(response) {
-  console.log("Poem generated");
-
   new Typewriter("#polish-poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -16,10 +14,12 @@ function generatePoem(event) {
   let apiKey = "9a4cbff04f4e654ca4teaa03bc88aoaf";
   let prompt = `User instructions: Generate a poem in Polish language about ${instructionsInput.value}`;
   let context =
-    "You are a romantic poem expert and love to write short poems. Your mission is to generatea 4 line poem in basic HTML. Always separate each line with <br />. Sign the poem as SheCodes AI inside the <strong> element at the end of the poem, and never at the beginning. Do not include any title to the poem. Make sure to follow the user instructions.";
+    "You are a romantic poem expert and love to write short poems. Your mission is to generatea 5 line poem in basic HTML. Each line should be separated. Sign the poem as SheCodes AI inside the <strong> element at the end of the poem, in a new line, and never at the beginning. Never include any title to the poem. Make sure to follow the user instructions.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem, please wait...");
+  let poemElement = document.querySelector("#polish-poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class = "blink">⏳ Generating the poem about ${instructionsInput.value} for you , please wait...</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
